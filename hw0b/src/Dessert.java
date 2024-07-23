4685ac2bfab22a0090273d4246c005f92fa94592
wyh <205653535@qq.com>
2024-07-23 16:55:58 +0800

    Finished task4(hw0b)

--- hw0b/src/Dessert.java
+++ hw0b/src/Dessert.java
@@ -1,3 +1,18 @@
 public class Dessert {
+    public int flover;
+    public int price;
+    public static int numDesserts;
+    public Dessert(int f, int p) {
+        flover = f;
+        price = p;
+        numDesserts += 1;
+    }
 
+    public void printDessert() {
+        System.out.print(flover + " " + price + " " + numDesserts);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("I love dessert!");
+    }
 }

d5720422abe6f1270b4b32430495e36b0b4e76ed
wyh <205653535@qq.com>
2024-07-26 10:29:21 +0800

    Defined Node

---
+++ proj1a/src/LinkedListDeque61B.java
@@ -0,0 +1,65 @@
+import java.util.List;
+
+public class LinkedListDeque61B<T> implements Deque61B<T> {
+
+
+    public class StuffNode<T> {
+        public T item;
+        public StuffNode prev;
+        public StuffNode next;
+
+        public StuffNode(T i, StuffNode p, StuffNode n) {
+            item = i;
+            prev = p;
+            next = n;
+        }
+    }
+    public LinkedListDeque61B() {
+
+    }
+
+    @Override
+    public void addFirst(T x) {
+
+    }
+
+    @Override
+    public void addLast(T x) {
+
+    }
+
+    @Override
+    public List<T> toList() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public T removeFirst() {
+        return null;
+    }
+
+    @Override
+    public T removeLast() {
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        return null;
+    }
+}
